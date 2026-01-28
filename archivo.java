dd150cc43535861b7d9eaedbbbd66f08b570e312
Mikel Valmaseda Garcia <ikmikelvalmaseda@H014451.NET>
2026-01-28 08:47:23 +0100

    Tercer commit

--- archivo.java
+++ archivo.java
@@ -1,6 +1,7 @@
 public class archivo{
     public static void main(String[] args) {
         System.out.println("Holaa"); 
-        System.out.println("Holaa");
+        System.out.println("Adios");
+        
     }
 }
