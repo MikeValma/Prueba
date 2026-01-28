074dbf249ef8a6ea4b87b31b94dc0534b9d67836
Mikel Valmaseda Garcia <ikmikelvalmaseda@H014451.NET>
2026-01-28 08:48:14 +0100

    Cuarto commit

--- archivo.java
+++ archivo.java
@@ -2,6 +2,7 @@ public class archivo{
     public static void main(String[] args) {
         System.out.println("Holaa"); 
         System.out.println("Adios");
+        System.out.println("Holaa");
         
     }
 }
